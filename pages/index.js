d98356bd6499b84638255786872a508903bdf700
ramenumai <taiki1151001@icloud.com>
2022-12-21 02:47:53 +0900

    delete p

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <Layout title="Home">
       <p className='text-4xl'>Welcome to Nextjs</p>
-      <p className='text-2xl mt-6'>please shom me my crazy video</p>
+      
     </Layout>
   );
 }
